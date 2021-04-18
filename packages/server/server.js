b8c9e9c4778fda30bab22adc83e024134733281f
Boyan Barakov <boyan.barakov@gmail.com>
2021-04-18 12:01:56 +0200

    server -> add addr in metadata

--- packages/server/server.js
+++ packages/server/server.js
@@ -4,21 +4,27 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 const express = require('express');
 const { ethers, Contract } = require('ethers');
 const app = express();
+
 app.use(express.static('.'));
 app.use(express.json());
 
 app.post('/create-intent', async (req, res) => {
-  const paymentIntent = await stripe.paymentIntents.create({
+  console.log(`User sig: ${req.body.sig}`);
+
+  const intent = await stripe.paymentIntents.create({
     amount: req.body.amount,
     currency: 'eur',
     payment_method_types: ['card'],
     transfer_group: '{ORDER10}',
     confirmation_method: 'manual',
     capture_method: 'manual',
+    metadata: {
+      addr: req.body.addr
+    },
   });
-  console.log(paymentIntent);
+  console.log(`Intent ${intent.id} created`);
 
-  res.json({ id: paymentIntent.id });
+  res.json({ id: intent.id });
 });
 
 //https://stripe.com/docs/payments/capture-later
@@ -34,6 +40,7 @@ app.post('/pay', async (req, res) => {
         payment_method: req.body.payment_method_id,
       }
     );
+    console.log(`Intent ${intent.id} paid`);
     res.json({ id: intent.id });
   } catch (e) {
     return res.send({ error: e.message });
@@ -46,38 +53,40 @@ app.get('/check', async (req, res) => {
       req.query.payment_intent_id
     );
     console.log(intent);
-    res.json({ SUCCESS: intent.status === 'requires_capture' });
+    console.log(`Intent ${intent.id} checked`);
+    const { status, metadata } = intent;
+    res.json({ SUCCESS: status === 'requires_capture' && metadata.addr === req.query.addr });
   } catch (e) {
     return res.send({ SUCCESS: false, error: e.message });
   }
 });
 
-app.get('/capture', async (req, res) => {
-  try {
-    const intent = await stripe.paymentIntents.capture(
-      req.query.payment_intent_id
-    );
-    console.log(intent);
-    res.send(true);
-  } catch (e) {
-    return res.send({ error: e.message });
-  }
-});
+//app.get('/capture', async (req, res) => {
+  //try {
+    //const intent = await stripe.paymentIntents.capture(
+      //req.query.payment_intent_id
+    //);
+    //console.log(intent);
+    //res.send(true);
+  //} catch (e) {
+    //return res.send({ error: e.message });
+  //}
+//});
 
-app.get('/release', async (req, res) => {
-  try {
-    const transfer = await stripe.transfers.create({
-      amount: 1000,
-      currency: 'eur',
-      destination: 'acct_1Ieke8RglQ4Yr66h',
-      transfer_group: '{ORDER10}',
-    });
-    console.log(transfer);
-    res.send(true);
-  } catch (e) {
-    return res.send({ error: e.message });
-  }
-});
+//app.get('/release', async (req, res) => {
+  //try {
+    //const transfer = await stripe.transfers.create({
+      //amount: 1000,
+      //currency: 'eur',
+      //destination: 'acct_1Ieke8RglQ4Yr66h',
+      //transfer_group: '{ORDER10}',
+    //});
+    //console.log(transfer);
+    //res.send(true);
+  //} catch (e) {
+    //return res.send({ error: e.message });
+  //}
+//});
 
 app.listen(4242, () => console.log('Running on port 4242'));
 
@@ -87,12 +96,10 @@ function listenForEvents() {
   const contract = new Contract("0x90076F213a376e4710839295E18f9aC69DFd92d2", abi, provider);
 
   contract.on('ReleasePaidTokens', async (addr, intent) => {
-    console.log(addr);
-    console.log(intent);
+    console.log(`Intent ${intent} captured.`);
     const res = await stripe.paymentIntents.capture(
       intent
     );
-    console.log(res);
   });
 }
 
